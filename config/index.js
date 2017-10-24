699ff2fbd682e810fd53e8e655150f3300516754
Yuemin.Hu <yuemin.hu@baidao.com>
2017-10-24 17:55:17 +0800

    change nodeenv

--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
 const config = {
-  baseURL: 'http://192.168.26.216:3000'
+  'development': {
+    baseURL: 'http://192.168.26.216:3000'
+  },
+  'production': {
+    baseURL: 'http://www.yoominhu.site'
+  }
 }
 
-export default config
+export default config[process.env.NODE_ENV || 'development']
